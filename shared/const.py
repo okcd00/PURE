da5af2a0177d69165275cb5c7575891c84062aa0
Chen Dian <okcd00@users.noreply.github.com>
2021-05-13 15:50:42 +0800

    Update const.py

--- shared/const.py
+++ shared/const.py
@@ -1,4 +1,7 @@
 task_ner_labels = {
+    'msra': [u"公司", u"人名", u"地址"],
+    'resume': [u"公司", u"人名", u"地址", u"学历", u"专业", u"国籍", u"民族", u"职称"],
+    'findoc': [u"公司", u"人名", u"地址", u"产品业务", u"文件"],
     'ace04': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
     'ace05': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
     'scierc': ['Method', 'OtherScientificTerm', 'Task', 'Generic', 'Material', 'Metric'],
