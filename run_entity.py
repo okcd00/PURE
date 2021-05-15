4844517905d66dbbcfc587bf1ec661c6ddc81ffc
Chen Dian <okcd00@users.noreply.github.com>
2021-05-15 11:48:03 +0800

    Update run_entity.py

--- run_entity.py
+++ run_entity.py
@@ -116,8 +116,7 @@ def setseed(seed):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--task', type=str, default=None, required=True, choices=['ace04', 'ace05', 'scierc', 'msra', 'resume', 'findoc'])
-
+    parser.add_argument('--task', type=str, default=None, required=True)
     parser.add_argument('--data_dir', type=str, default=None, required=True, 
                         help="path to the preprocessed dataset")
     parser.add_argument('--output_dir', type=str, default='entity_output', 
